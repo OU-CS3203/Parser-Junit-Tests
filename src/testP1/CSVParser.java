0fffb5246386113788479d5946e4d3b59b28fed3
DraperRiley <rileyldraper@gmail.com>
2022-01-26 16:36:44 -0600

    added translation for csv files

--- src/testP1/CSVParser.java
+++ src/testP1/CSVParser.java
@@ -1,20 +1,29 @@
 package testP1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.SortedMap;
+import com.github.pemistahl.lingua.api.Language;
+import com.github.pemistahl.lingua.api.LanguageDetector;
+import com.github.pemistahl.lingua.api.LanguageDetectorBuilder;
+import static com.github.pemistahl.lingua.api.Language.ENGLISH;
+import static com.github.pemistahl.lingua.api.Language.SPANISH;
 import java.io.File;
 import java.io.FileNotFoundException;;
 
 public class CSVParser {
 	
 	private Scanner in;
+	final private LanguageDetector detector = LanguageDetectorBuilder.fromLanguages(ENGLISH, SPANISH).build();
 	
 	public CSVParser(File csvFile) throws FileNotFoundException {
 		
 		try {
 			in = new Scanner(csvFile);
+			in.useDelimiter(",");
 		}
 		catch(Exception e) {
-			System.out.print("File not found \n");
+			throw new FileNotFoundException();
 		}
 		
 	}
@@ -25,5 +34,44 @@ public class CSVParser {
 		return temp.split(",");
 	}
 	
+	public String next() {
+		String temp = in.next();
+		return temp;
+	}
+	
+	public ArrayList<String> translateCSV(){
+		
+		ArrayList<String> translatedList = new ArrayList<String>();
+		
+		
+		while(in.hasNext()) {
+			
+			String temp = "";
+			temp = in.next();
+			SortedMap<Language, Double> tempMap = detector.computeLanguageConfidenceValues(temp);
+			
+				
+			if(!tempMap.isEmpty()) {
+				if(tempMap.containsKey(ENGLISH) && tempMap.get(ENGLISH) > 0.9) {
+					temp += ",en";
+				}
+				if(tempMap.containsKey(SPANISH) && tempMap.get(SPANISH) > 0.9) {
+					temp += ",es";
+				}	
+			}
+			else {
+				temp += ",Unknown";
+			}
+			
+			translatedList.add(temp);
+			
+		}
+		
+		return translatedList;
+	}
+	
+	public void close() {
+		in.close();
+	}
 	
 }
