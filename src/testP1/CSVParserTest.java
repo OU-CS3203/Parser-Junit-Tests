d43333ad4d39753cf6f9dce482931263b8eec831
DraperRiley <rileyldraper@gmail.com>
2022-01-27 00:54:28 -0600

    junit tests

---
+++ src/testP1/CSVParserTest.java
@@ -0,0 +1,71 @@
+package testP1;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Random;
+
+public class CSVParserTest {
+
+	@Before
+	public void setUp() throws Exception {
+		CSVParser parser;
+	}
+
+	@Test
+	public void testCognate() {
+		CSVParser parser = new CSVParser("global,total");
+		ArrayList<String> temp = parser.translateCSV();
+		String[] answer = {"global,en,es", "total,en,es"};
+		assertTrue(temp.get(0).equals(answer[0]) && temp.get(1).equals(answer[1]));
+	}
+	
+	@Test
+	public void testNumber() {
+		
+		final int upperbound = 100000;
+		Random rand = new Random();
+		int[] randInt = {rand.nextInt(upperbound), rand.nextInt(upperbound)};
+		String parseString = "" + randInt[0] + "," + randInt[1];
+		CSVParser parser = new CSVParser(parseString);
+		
+		String[] answer = {"" + randInt[0] + ",Unknown", "" + randInt[1] + ",Unknown"};
+		ArrayList<String> temp = parser.translateCSV();
+		
+		assertTrue(temp.get(0).equals(answer[0]) && temp.get(1).equals(answer[1]));
+		
+	}
+	
+	@Test
+	public void testTranslate() {
+		
+		CSVParser parser = new CSVParser("University,Dónde");
+		ArrayList<String> temp = parser.translateCSV();
+		String[] answer = {"University,en", "Dónde,es"};
+		
+		assertTrue(temp.get(0).equals(answer[0]) && temp.get(1).equals(answer[1]));
+		
+	}
+
+	@Test
+	public void testOrder() throws FileNotFoundException {
+		File file = new File("src\\testP1\\testcsv\\testOrder.csv");
+		CSVParser parser = new CSVParser(file);
+		String[] answer = {"Talent","Purpose","Guatemala","Tower","Television","1024","Hatsune Miku","Keyboard","Por qué"};
+		int bool = 1;
+		
+		for(int i = 0; parser.hasNext(); ++i) {
+			if(!parser.next().equals(answer[i])) {
+				bool = 0;
+			}
+		}
+		
+		assertTrue(bool == 0);
+		
+	}
+	
+}
